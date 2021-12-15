9da6dc1f1842d7c68b218223c4015101001116c5
Ujjal <khadkauzal@gmail.com>
2021-12-15 23:33:57 +0545

    created-the-player-and-added-player-movement

--- level.py
+++ level.py
@@ -1,7 +1,9 @@
 import pygame
+from player import Player
 
 from tiles import Tile
 from settings import tile_size
+from player import Player
 
 
 class Level:
@@ -13,15 +15,26 @@ class Level:
 
     def setup_level(self, layout_data):
         self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
 
         for row_index, row in enumerate(layout_data):
             for col_index, col in enumerate(row):
+                x = col_index * tile_size
+                y = row_index * tile_size
+
                 if col == 'X':
-                    x = col_index * tile_size
-                    y = row_index * tile_size
                     tile = Tile((x, y), tile_size)
                     self.tiles.add(tile)
 
+                elif col == 'P':
+                    player_sprite = Player((x, y))
+                    self.player.add(player_sprite)
+
     def run(self):
+      # level tiles
         self.tiles.update(self.world_shift)  # update the tiles horizontally
         self.tiles.draw(self.display_surface)
+
+        # player
+        self.player.update()  # update the player
+        self.player.draw(self.display_surface)
