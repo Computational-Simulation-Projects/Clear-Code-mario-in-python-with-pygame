3b1b12ccb90bf9303bc4f10e7c4170a21cb2906e
Ujjal <khadkauzal@gmail.com>
2021-12-16 01:11:34 +0545

    implemented-player-movement-and-collisions

--- level.py
+++ level.py
@@ -47,13 +47,39 @@ class Level:
 
         self.tiles.update(self.world_shift)
 
+    def horizontal_movement_collision(self):
+        player = self.player.sprite
+        player.rect.x += player.direction.x * player.speed
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.x < 0:
+                    player.rect.left = sprite.rect.right
+                elif player.direction.x > 0:
+                    player.rect.right = sprite.rect.left
+
+    def vertical_movement_collision(self):
+        player = self.player.sprite
+        player.apply_gravity()
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.y > 0:
+                    player.rect.bottom = sprite.rect.top
+                    player.direction.y = 0  # to cancel the cumulating gravity
+                elif player.direction.y < 0:
+                    player.rect.top = sprite.rect.bottom
+                    player.direction.y = 0  # to cencel the any negative y-direction movement
+
     def run(self):
         # level tiles
         self.tiles.update(self.world_shift)  # update the tiles horizontally
         self.tiles.draw(self.display_surface)
+        self.scroll_x()
 
         # player
         self.player.update()  # update the player
-        self.player.draw(self.display_surface)
+        self.horizontal_movement_collision()
+        self.vertical_movement_collision()
 
-        self.scroll_x()
+        self.player.draw(self.display_surface)
